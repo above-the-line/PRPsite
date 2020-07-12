2a9844c58fbd3e2dd4925b1bfe0aeb23fe6af323
Avi Michael <play.right.productions@gmail.com>
2020-07-12 13:21:48 +1000

    spelling

--- README.md
+++ README.md
@@ -2,17 +2,24 @@
 
 To Get Started:
 
-1. Git clone https://github.com/above-the-line/PRPsite.git
-2. npm install
-3. If you have any running MongoDB instances, delete:
-   a) prisma
+1. CLONE SOURCE CODE
+   Git clone https://github.com/above-the-line/PRPsite.git
+2. INSTALL SOURCE CODE DEPENDENCIES
+   npm install
+3. ENSURE THAT NO DATABASE NAMES WILL CONFLICT
+   If you have any running MongoDB instances, delete:
+   a) prismazxc
    b) default_default
-4. create .env file with vars:
-   a) $MONGO_INITDB_ROOT_USERNAME=prisma
-   b) $MONGO_INITDB_ROOT_PASSWORD=prisma
-5. (in ./) docker-compose --file stack.yml up
-6. (in ./prisma) prisma deploy
-7. (in ./src) node index.js
+4. SET UP YOUR ENVIRONMENT VARIABLES / SECRETS
+   create .env file with vars:
+   a) MONGO_INITDB_ROOT_USERNAME=mysecretname
+   b) MONGO_INITDB_ROOT_PASSWORD=mysecretpassword
+5. SPIN UP THE ENVIRONMENT IN DOCKER
+   (in ./) docker-compose --file stack.yml up
+6. DEPLOY THE PRISMA CLIENT
+   (in ./prisma) prisma deploy
+7. DEPLOY THE GQL SERVER (GQL-YOGA)
+   (in ./src) node index.js
 
 ---
 
--- src/index.js
+++ src/index.js
@@ -1,42 +1,38 @@
-const ENV_VARS = require('./env_vars.js')
-const { prisma } = require('../generated/prisma-client')
-const { GraphQLServer } = require('graphql-yoga')
-const Query = require('../src/resolvers/Query')
-const Mutation = require('../src/resolvers/Mutation')
-const Project = require('../src/resolvers/Project')
-
-
+const ENV_VARS = require("./env_vars.js");
+const { prisma } = require("../generated/prisma-client");
+const { GraphQLServer } = require("graphql-yoga");
+const Query = require("../src/resolvers/Query");
+const Mutation = require("../src/resolvers/Mutation");
+const Project = require("../src/resolvers/Project");
 
 const resolvers = {
   Query,
   Mutation,
-  Project
-}
-
+  Project,
+};
 
 // In order to access the request object in utils
 /*
 To enable data to be sent to and returned from the context object,
 it is created as a function literal that returns it's argument,
 plus other things, like the auto-generated prisma client, 
-This allows currying. So When the HTTP request carrying the incoming 
-GraphQL query (or mutation) to the context as well.
+This allows currying. So you can functionally "decorate"
+the HTTP request carrying the incoming GraphQL query (or mutation) 
+that is travelling to / from the context.
 This will allow your resolvers to read the Authorization header 
 and validate if the user who submitted the request is eligible to perform the requested operation.
 */
 
-
-
 const server = new GraphQLServer({
-    typeDefs: './schema.graphql',
-    resolvers,
-    context: response => {
-      return {
-      	...response,
-      	prisma,
-      }
-    },
-})
+  typeDefs: "./schema.graphql",
+  resolvers,
+  context: (response) => {
+    return {
+      ...response,
+      prisma,
+    };
+  },
+});
 
-console.log(process.env.UTILS_APP_SECRET)
-server.start(() => console.log('Server is running on http://localhost:4000'))
+console.log(process.env.UTILS_APP_SECRET);
+server.start(() => console.log("Server is running on http://localhost:4000"));
